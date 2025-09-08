c8d74877783821328c8185ccd793b6411fd49bb3
yordan-gergov01 <yordansoftskills@gmail.com>
2025-09-08 12:38:09 +0300

    added sidebar with new project form with styled fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import NewProject from "./features/projects/NewProject";
+import Sidebar from "./ui/Sidebar";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <Sidebar />
+      <NewProject />
+    </main>
   );
 }
 
